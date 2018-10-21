0b17349f512a17c82b98377af95ee0918386967b
Emir Korkmaz <emir.korkmaz@payrov.com.tr>
2018-10-21 20:34:25 +0300

    initial

---
+++ src/main/java/com/study/sb/microservices/currencyconversion/sbmicroservicescurrencyconversion/SbMicroservicesCurrencyconversionApplication.java
@@ -0,0 +1,12 @@
+package com.study.sb.microservices.currencyconversion.sbmicroservicescurrencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMicroservicesCurrencyconversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMicroservicesCurrencyconversionApplication.class, args);
+	}
+}
